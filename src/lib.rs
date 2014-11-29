c9a04166038377060314dbe65b3f33005b8ae7b2
CodeRedTails <dodofxp@gmail.com>
2014-11-29 10:45:08 +0000

    Introduces WinnerResult enum for matching on win
    WinnerResult can be used to check for a winning line and
    get the winner in one go

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,31 @@
 pub mod line {
+    pub struct Line(Player, Player, Player);
+
+    #[deriving(PartialEq)]
     pub enum Player {
         X,
         O,
-        None
+        Empty,
     }
-    pub struct Line(Player, Player, Player);
 
-    pub fn has_winner(line: Line) -> bool {
+    pub enum WinnerResult {
+        Winner(Player),
+        NoWinner,
+    }
+
+    pub fn has_winner(line: Line) -> WinnerResult {
         match line {
-            Line(Player::X, Player::X, Player::X) => true,
-            Line(Player::O, Player::O, Player::O) => true,
-            Line(_,_,_)         => false
+            Line(Player::X, Player::X, Player::X) => WinnerResult::Winner(Player::X),
+            Line(Player::O, Player::O, Player::O) => WinnerResult::Winner(Player::O),
+            Line(_,_,_)                           => WinnerResult::NoWinner,
         }
     }
 
     pub fn empty() -> Line {
-        new(Player::None, Player::None, Player::None)
+        new(Player::Empty, Player::Empty, Player::Empty)
     }
 
     pub fn new(first: Player, second: Player, third: Player) -> Line {
         Line(first, second, third)
     }
 }
-
--- tests/board_spec.rs
+++ tests/board_spec.rs
@@ -3,27 +3,45 @@
 extern crate tic_tac_toe;
 
 use tic_tac_toe::line::Player;
+use tic_tac_toe::line::WinnerResult;
+use tic_tac_toe::line::Line;
+use tic_tac_toe::line;
 
 #[test]
 fn line_with_three_x_has_winner() {
     let line  = tic_tac_toe::line::new(Player::X, Player::X, Player::X);
-    assert!(tic_tac_toe::line::has_winner(line));
+    assert!(is_winner_of_line(line, Player::X));
+    assert!(!is_winner_of_line(line, Player::O));
 }
 
 #[test]
 fn line_with_three_o_has_winner() {
     let line  = tic_tac_toe::line::new(Player::O, Player::O, Player::O);
-    assert!(tic_tac_toe::line::has_winner(line));
+    assert!(is_winner_of_line(line, Player::O));
+}
+
+fn is_winner_of_line(line: Line, player: Player) -> bool {
+    match line::has_winner(line) {
+        WinnerResult::Winner(n) if n == player => true,
+                                             _ => false,
+    }
 }
 
 #[test]
 fn empty_line_has_no_winner() {
     let line  = tic_tac_toe::line::empty();
-    assert!(!tic_tac_toe::line::has_winner(line));
+    assert!(has_no_winner(line));
+}
+
+fn has_no_winner(line: Line) -> bool {
+    match line::has_winner(line) {
+        WinnerResult::NoWinner => true,
+        _ => false,
+    }
 }
 
 #[test]
 fn mixed_line_has_no_winner() {
     let line  = tic_tac_toe::line::new(Player::X, Player::O, Player::X);
-    assert!(!tic_tac_toe::line::has_winner(line))
+    assert!(has_no_winner(line));
 }
