9867685f48bcea7fa48e06931f60ed9701463624
CodeRedTails <dodofxp@gmail.com>
2014-11-30 13:43:38 +0000

    Allows a board to be rendered using a display

--- src/io/display.rs
+++ src/io/display.rs
@@ -1,7 +1,23 @@
 use line::Player;
+use board::Board;
 
-pub fn render_line(line: &[Player]) -> String {
-    line.iter().enumerate().map(render_cell).collect::<Vec<String>>().concat()
+pub fn render(board: Board) -> Vec<String> {
+    let rows = board.rows();
+    let mut result = Vec::new();
+    let mut offset = 0u;
+    for row in rows.iter() {
+        let line = render_line(*row, offset);
+        offset += 3;
+        result.push(line);
+    }
+    result
+}
+
+pub fn render_line(line: &[Player], offset: uint) -> String {
+    line.iter().enumerate()
+               .map(|(idx, player)| render_cell((idx+offset, player)))
+               .collect::<Vec<String>>()
+               .concat()
 }
 
 pub fn render_cell(elements: (uint, &Player)) -> String {
--- tests/display_spec.rs
+++ tests/display_spec.rs
@@ -6,10 +6,25 @@ use tic_tac_toe::io;
 use tic_tac_toe::line::Player;
 
 #[test]
+fn prints_a_board_into_strings_per_row() {
+    let first  = "[0][1][2]".to_string();
+    let second = "[3][4][5]".to_string();
+    let third  = "[6][7][8]".to_string();
+    let result: Vec<String> = vec![first, second, third];
+
+    let board = tic_tac_toe::board::empty();
+    assert!(io::display::render(board) == result);
+}
+
+#[test]
 fn prints_an_empty_row_of_board() {
-    let line = [Player::Empty, Player::Empty, Player::Empty];
-    let result = io::display::render_line(&line);
+    let line = empty();
+    let result = io::display::render_line(&line,0);
     assert_eq!("[0][1][2]".to_string(), result);
+}
+
+fn empty() -> [Player, ..3] {
+    [Player::Empty, Player::Empty, Player::Empty]
 }
 
 #[test]
