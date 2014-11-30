b2cf8e242d7680bb0a04d4bcf56854492871b888
CodeRedTails <dodofxp@gmail.com>
2014-11-30 10:50:19 +0000

    Reorders parameters in render_cell to match tuple

--- src/io/display.rs
+++ src/io/display.rs
@@ -1,17 +1,12 @@
 use line::Player;
 
-pub fn render(line: &[Player,..3]) -> String {
-    let transformed_line: Vec<String> = line.iter().enumerate().map(render_tuple).collect();
+pub fn render_line(line: &[Player,..3]) -> String {
+    let transformed_line: Vec<String> = line.iter().enumerate().map(render_cell).collect();
     transformed_line.connect("")
 }
 
-
-fn render_tuple(elements: (uint, &Player)) -> String {
+pub fn render_cell(elements: (uint, &Player)) -> String {
     let (idx, player) = elements;
-    render_cell(player, idx)
-}
-
-pub fn render_cell(player: &Player, idx: uint) -> String {
     match *player {
         Player::X => "[X]".to_string(),
         Player::O => "[O]".to_string(),
--- tests/display_spec.rs
+++ tests/display_spec.rs
@@ -5,31 +5,30 @@ extern crate tic_tac_toe;
 use tic_tac_toe::io;
 use tic_tac_toe::line::Player;
 
-#[ignore]
 #[test]
 fn prints_an_empty_row_of_board() {
     let line = [Player::Empty, Player::Empty, Player::Empty];
-    let result = io::display::render(&line);
+    let result = io::display::render_line(&line);
     assert_eq!("[0][1][2]".to_string(), result);
 }
 
 #[test]
 fn prints_player_x() {
     let player = Player::X;
-    let result = io::display::render_cell(&player, 0u);
+    let result = io::display::render_cell((0u, &player));
     assert_eq!("[X]".to_string(), result);
 }
 
 #[test]
 fn prints_player_o() {
     let player = Player::O;
-    let result = io::display::render_cell(&player, 0u);
+    let result = io::display::render_cell((0u,&player));
     assert_eq!("[O]".to_string(), result);
 }
 
 #[test]
 fn prints_index_when_empty() {
     let player = Player::Empty;
-    let result = io::display::render_cell(&player, 1u);
+    let result = io::display::render_cell((1u, &player));
     assert_eq!("[1]".to_string(), result);
 }
