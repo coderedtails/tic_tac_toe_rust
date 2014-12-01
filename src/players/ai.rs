391746f9a1f6b3fec5b104e23fc835acddecf6c4
CodeRedTails <dodofxp@gmail.com>
2014-12-01 11:45:26 +0000

    Stub out methods for alpha-beta-negamax AI
    This is based on my previous Java implementation.

--- src/players/ai.rs
+++ src/players/ai.rs
@@ -17,6 +17,29 @@ impl Ai {
     pub fn best_move(&self, board: Board) -> uint {
         1u
     }
+
+    fn negamax(board: Board, alpha: int, beta: int, name: Marker) -> int {
+        if board.is_finished() {
+            Ai::value_of_board(board, name)
+        } else {
+            1
+        }
+    }
+
+    fn value_of_board(board: Board, name: Marker) -> int {
+      if board.is_winner(&name) {
+          board.value()
+      } else {
+         -board.value()
+      }
+    }
+
+    fn score_unfinshed(board: Board, alpha: int, beta: int, name: Marker) -> int {
+        let best_score = alpha;
+        for m in board.remaining_moves().iter() {
+        }
+        0
+    }
 }
 
 pub fn new(name: Marker) -> Ai {
