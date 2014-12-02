930d50769fdfafa14a8e154f3f402215627642e0
CodeRedTails <dodofxp@gmail.com>
2014-12-02 13:45:43 +0000

    Change recursive make_move into loop

--- src/players/human.rs
+++ src/players/human.rs
@@ -13,12 +13,12 @@ pub struct Human<P> {
 impl<P: IO> Player for Human<P> {
     fn make_move(&self, board: Board) -> Board{
         let possible_moves = board.remaining_moves();
-        let choice = self.display.request_move();
 
-        if possible_moves.contains(&choice) {
-            board.make_move(choice, &self.name)
-        } else {
-            self.make_move(board)
+        loop {
+            let choice = self.display.request_move();
+            if possible_moves.contains(&choice) {
+                return board.make_move(choice, &self.name)
+            }
         }
     }
 }
