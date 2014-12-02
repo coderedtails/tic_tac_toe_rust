6a1aeb94c9fb55d9b612c4e3fe3ad41f65b8a220
CodeRedTails <dodofxp@gmail.com>
2014-12-02 15:18:54 +0000

    Creates a scripted player for tests

--- src/players/mod.rs
+++ src/players/mod.rs
@@ -3,6 +3,7 @@ use core::board::Board;
 pub mod ai;
 pub mod human;
 pub mod game_mode;
+pub mod scripted_player;
 
 pub trait Player {
     fn make_move(&self, board: Board) -> Board;
---
+++ src/players/scripted_player.rs
@@ -0,0 +1,25 @@
+use std::cell::RefCell;
+
+use core::board::Board;
+use core::marker::Marker;
+use players::Player;
+
+pub struct ScriptedPlayer {
+    pub name: Marker,
+    pub moves: RefCell<Vec<uint>>,
+}
+
+impl Player for ScriptedPlayer {
+    fn make_move(&self, board: Board) -> Board{
+        match self.moves.borrow_mut().pop() {
+            Some(n) => board.make_move(n, &self.name),
+            None => panic!("No moves left!"),
+        }
+    }
+}
+
+pub fn new_with_moves(name: Marker, moves: Vec<uint>) -> ScriptedPlayer {
+    ScriptedPlayer {  name: name,
+                      moves: RefCell::new(moves) }
+}
+
