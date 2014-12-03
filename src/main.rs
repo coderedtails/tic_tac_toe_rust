0f50ed09899a47879b868dd8e21d2ba7a21209e3
CodeRedTails <dodofxp@gmail.com>
2014-12-03 19:35:35 +0000

    Create all variations of game modes

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
 extern crate tic_tac_toe;
 
-use tic_tac_toe::players::ai::Ai;
-use tic_tac_toe::players::human::Human;
-use tic_tac_toe::core::marker::Marker;
 use tic_tac_toe::io::display::Display;
-use tic_tac_toe::io::cli::Cli;
 use tic_tac_toe::io::cli;
 use tic_tac_toe::game::game::Game;
-use tic_tac_toe::players::game_mode::GameMode;
-
-use std::cell::Cell;
+use tic_tac_toe::players::game_mode;
 
 fn main() {
 
     let display = Display { cli: cli::new(), use_colour: true};
-    let game_mode = choose_game(display);
+    let game_mode = game_mode::choose_game_mode(display);
 
     let game = Game { display: display};
     game.play(game_mode);
-}
-
-
-fn choose_game<'a>(display: Display<Cli>) -> GameMode<'a> {
-    let ai =  Ai { name: Marker::O };
-    let human = Human { name: Marker::X, display: display };
-    GameMode { first: box human, second: box ai, counter: Cell::new(0u) }
 }
 
--- src/players/game_mode.rs
+++ src/players/game_mode.rs
@@ -1,5 +1,10 @@
 use players::Player;
 use std::cell::Cell;
+use io::display::Display;
+use io::cli::Cli;
+use core::marker::Marker;
+use players::ai::Ai;
+use players::human::Human;
 
 pub struct GameMode<'a>{
     pub first:  Box<Player + 'a>,
@@ -25,4 +30,45 @@ impl<'a> GameMode<'a> {
     pub fn opponents_line(&self) -> String {
         format!("{} vs. {}", self.first.player_type(), self.second.player_type())
     }
+}
+
+pub fn new<'a>(first: Box<Player + 'a>, second: Box<Player + 'a>) -> GameMode<'a> {
+    GameMode { first: first, second: second, counter: Cell::new(0u) }
+}
+
+pub fn choose_game_mode<'a>(display: Display<Cli>) -> GameMode<'a> {
+    let possible_modes = create_game_modes(display);
+
+    possible_modes[0]
+}
+
+pub fn create_game_modes<'a>(display: Display<Cli>) -> [GameMode<'a>, ..4] {
+    [human_vs_human(display),
+     ai_vs_ai(display),
+     human_vs_ai(display),
+     ai_vs_human(display)]
+}
+
+fn human_vs_ai<'a>(display: Display<Cli>) -> GameMode<'a> {
+    let human = Human { name: Marker::X, display: display };
+    let ai =  Ai { name: Marker::O };
+    new(box ai ,box human)
+}
+
+fn ai_vs_human<'a>(display: Display<Cli>) -> GameMode<'a> {
+    let ai =  Ai { name: Marker::X };
+    let human = Human { name: Marker::O, display: display };
+    new(box human, box ai)
+}
+
+fn human_vs_human<'a>(display: Display<Cli>) -> GameMode<'a> {
+    let human1 = Human { name: Marker::X, display: display };
+    let human2 = Human { name: Marker::O, display: display };
+    new(box human1,box human2)
+}
+
+fn ai_vs_ai<'a>(display: Display<Cli>) -> GameMode<'a> {
+    let ai1 = Ai { name: Marker::X};
+    let ai2 = Ai { name: Marker::O};
+    new(box ai1,box ai2)
 }
